ea4846f2c951abd88668e23ab7034f2206209d6a
Muhammad Abubakar <abubakar60675@gmail.com>
2026-04-17 03:03:47 +0500

    Changes

--- 01-URL Shortener/index.js
+++ 01-URL Shortener/index.js
@@ -1,12 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { connectToMongoDB } from "./db.js";
 import urlRoute from "./routes/url.js";
 import { handleGetOriginalURL } from "./controllers/url.js";
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT;
 
-connectToMongoDB("mongodb://localhost:27017/url-shortener").then(() => {
+connectToMongoDB(process.env.MONGODB_URL).then(() => {
   console.log("MongoDB connected");
 });
 app.use(express.json());
